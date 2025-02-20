9527a85f388ac0b5ed4d341bcde969264efee277
novanish <anishchhetri196@gmail.com>
2025-02-20 19:42:14 +0545

    fix(mail): Use correct invoice link in email notifications

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -10,6 +10,7 @@ function getSender() {
 
 interface EmailOptions {
   to: Array<{ email: string; name?: string }>;
+  invoiceId: string;
   inoviceData: Record<
     "clientName" | "invoiceNumber" | "dueDate" | "totalAmount",
     string
@@ -19,18 +20,19 @@ interface EmailOptions {
 export async function sendInvoiceCreatedEmail({
   inoviceData,
   to,
+  invoiceId,
 }: EmailOptions) {
+  const inoviceURL = new URL(process.env.ORIGIN!);
+  inoviceURL.pathname = `/app/invoices/${invoiceId}`;
+
   await mailClient.send({
     from: getSender(),
-    subject: "Invoice created",
-    text: "Your invoice has been created",
-    category: "invoice-created",
     to,
 
     template_uuid: "c21f0735-0a7c-4e72-9009-2ded2dd4746a",
     template_variables: {
       ...inoviceData,
-      inoviceLink: "https://example.com/invoice/1",
+      invoiceLink: inoviceURL.toString(),
     },
   });
 }
